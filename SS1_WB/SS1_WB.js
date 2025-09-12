59784bd9897d0ea10eb07db1bcfae21f81b9d24b
WilmanBonilla <wilmanuzumaki@gmail.com>
2025-09-12 10:43:11 -0400

    Add files via upload

---
+++ SS1_WB/SS1_WB.js
@@ -0,0 +1,38 @@
+//wilman bonilla                                                     Sep 11 2025
+// I am making a playground with kids playing around it//
+// PLAYGROUND FUN
+
+
+
+function setup() {
+  createCanvas(1920, 1080);
+
+//set backgrond color (sky)
+
+background(98, 250, 240, 67); 
+
+
+//I used a rectangle to make the grass
+fill(16, 250, 1, 98);
+rect(0, 450, 1980, 670);
+
+//sun 
+fill(255, 240, 89, 100);
+ellipse(200, 100, 150, 150);
+
+
+//sandbox
+strokeWeight(5)
+fill(255, 250, 170, 100);
+rect(650, 650, 200, 100);
+
+
+
+rect(100,300,30,80);
+
+
+
+}
+
+
+
