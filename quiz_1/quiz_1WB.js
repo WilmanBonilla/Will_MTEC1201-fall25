812cdc489bfd631c6c744354877c9a57da85f505
WilmanBonilla <wilmanuzumaki@gmail.com>
2025-09-12 12:02:15 -0400

    Add files via upload

---
+++ quiz_1/quiz_1WB.js
@@ -0,0 +1,21 @@
+//Wilman Bonilla 
+//Quiz1
+//sep 12 2025
+
+function setup() {
+  createCanvas(800, 600);
+
+//set background 
+
+background(127, 125);
+
+//set line 
+
+line(0, 0, 400, 300);
+
+
+
+
+
+}
+
