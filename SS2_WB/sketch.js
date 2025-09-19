883928d25cf74b9a28b13fc8fedf17b7e4d82bb5
WilmanBonilla <wilmanuzumaki@gmail.com>
2025-09-18 23:53:39 -0400

    Add files via upload

---
+++ SS2_WB/sketch.js
@@ -0,0 +1,177 @@
+//wilman bonilla                                                     Sep 11 2025
+// I am making a playground with kids playing around it//
+// PLAYGROUND FUN
+
+
+//I am using let to set my variable 
+
+
+ let ML=100
+
+
+
+
+function setup() {
+  createCanvas(1920, 1080);
+
+//set backgrond color (sky)
+
+background(98, 250, 240, 67); 
+
+
+//I used a rectangle to make the grass
+fill(16, 250, 1, );
+rect(0, 450, 1980, 670);
+
+//sun 
+fill(255, 240, 89,);
+ellipse(200, 100, 150, 150);
+
+
+//sandbox
+strokeWeight(5)
+fill(255, 250, 170);
+rect(650, 650, 200, 100);
+
+
+
+
+
+
+// I am adding more things to my drawing
+
+
+/// pull up bar
+
+stroke(5);
+fill(245, 56, 0);
+rect(400, 400, 30, 400);
+
+stroke(5);
+fill(245, 56, 0);
+rect(100, 400, 30, 400);
+
+
+stroke(5);
+line(400, 400, 100, 400);
+
+
+
+
+// rocketship
+
+triangle(30, 75, 58, 20, 86, 75);
+
+
+
+
+
+
+}
+
+function draw() {
+
+//using the Let com
+
+
+// arms
+fill(252, 189, 113);
+rect(ML, ML, 30, 100);
+rect(ML, ML, 30, 100);
+
+
+//body
+fill(122, 255, 233);
+rect(ML, ML, 55, 55);
+rect(ML, ML, 200, 150);
+
+//legs
+fill(252, 189, 113); 
+rect(ML, ML, 30, 100);
+rect(ML, ML, 30, 100);
+
+//shoes 
+fill(0,0,0);
+rect(ML, ML, 80, 30);
+rect(ML, ML, 80, 30);
+
+ML = ML + 1;
+
+
+
+
+
+  //this code is is you want to move billy with the mouse 
+//background(98, 250, 240, 67); 
+
+// arms
+//fill(252, 189, 113);
+//rect(Mousex, mouseY, 30, 100);
+//rect(Mousex, mouseY, 30, 100);
+
+
+//body
+//fill(122, 255, 233);
+//rect(Mousex, mouseY, 55, 55);
+//rect(Mousex, mouseY, 200, 150);
+
+//legs
+//fill(252, 189, 113); 
+//rect(Mousex, mouseY, 30, 100);
+//rect(Mousex, mouseY, 30, 100);
+
+//shoes 
+//fill(0,0,0);
+//rect(mouseX, mouseY, 80, 30);
+//rect(Mousex, mouseY, 80, 30);
+
+
+
+
+//This is what billy looks like just in case the moving doesnt work.
+
+
+// arms
+//fill(252, 189, 113);
+//rect(350, 400, 30, 100);
+//rect(200, 400, 30, 100);
+
+
+//body
+//fill(122, 255, 233);
+//rect(265, 445, 55, 55);
+//rect(185, 500, 200, 150);
+
+//legs
+//fill(252, 189, 113); 
+//rect(350, 650, 30, 100);
+//rect(200, 650, 30, 100);
+
+//shoes 
+//fill(0,0,0);
+//rect(350, 720, 80, 30);
+//rect(200, 720, 80, 30);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
